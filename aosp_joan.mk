b73d7d8fcb2007db848619fdd95f9a83e3eea8bc
Slowhy <we@jcts.pw>
2021-03-06 19:24:47 +0800

    joan: PE-ify

--- aosp_joan.mk
+++ aosp_joan.mk
@@ -6,7 +6,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
@@ -19,20 +18,23 @@ $(call inherit-product, device/lge/joan/device.mk)
 
 # Device identifiers
 PRODUCT_DEVICE := joan
-PRODUCT_NAME := lineage_joan
+PRODUCT_NAME := aosp_joan
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := joan
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RELEASE_NAME := V30
 
 PRODUCT_GMS_CLIENTID_BASE := android-om-lg
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_GAPPS_ARCH := arm64
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="joan" \
     PRODUCT_DEVICE="joan" \
     PRODUCT_NAME="joan_global_com" \
-    PRIVATE_BUILD_DESC="joan_global_com-user 9 PKQ1.190414.001 192451212666b release-keys"
+    PRIVATE_BUILD_DESC="joan_global_com-user 8.0.0 OPR1.170623.026 181381736b4e9 release-keys"
 
-BUILD_FINGERPRINT="lge/joan_global_com/joan:9/PKQ1.190414.001/192451212666b:user/release-keys"
+BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/181381736b4e9:user/release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model ro.product.system.model
